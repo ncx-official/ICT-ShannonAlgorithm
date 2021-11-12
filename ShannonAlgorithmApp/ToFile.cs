25f31cf37bcd588221de5dddda1041de3b8aa5d4
litl-program <72743791+litl-program@users.noreply.github.com>
2021-11-12 20:32:01 +0200

    ToFile

---
+++ ShannonAlgorithmApp/ToFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShannonAlgorithmApp
+{
+    class ToFile
+    {
+        private string path = @"C:\";
+        private string name;
+
+        public ToFile(string name = "Test")
+        {
+            this.name = name;
+            path = path + $"{name}.txt";
+        }
+        
+        public void WriteToFile(Letter let)
+        {
+            try
+            {
+                File.AppendAllText(path, $"{let.letter} {let.count} {let.interest} \n");
+            }
+            catch
+            {
+                MessageBox.Show("Error in File");
+                return;
+            }
+        }
+    }
+}
